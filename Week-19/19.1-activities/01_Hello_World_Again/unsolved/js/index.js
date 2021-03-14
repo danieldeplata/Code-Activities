3825ee5c98d3586ba7c31e10a54dc49c1914fc49
danieldeplata <deplata.daniel@gmail.com>
2021-03-13 16:33:14 -0800

    Push 1
    Updates on Week 18 Activities and Week 19.1 Activities

---
+++ Week-19/19.1-activities/01_Hello_World_Again/unsolved/js/index.js
@@ -0,0 +1,11 @@
+// Create a variable below
+var helloWorld = "Hello World!";
+var number = '777';
+// Console.log() your variable here.
+console.log(helloWorld);
+console.log(number);
+
+var x = 20;
+var y = 10;
+var sum = x + y;
+var label = "My NUmber";
